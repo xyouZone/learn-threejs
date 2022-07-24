78190790535e8a27ca8c35ac73a753534a0d07ef
zxyou <1650567329@qq.com>
2022-07-24 19:13:27 +0800

    feat 添加轨道控制器

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { TEngine } from './assets/ts/TEngine';
+import { defineComponent,onMounted,ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const threeTarget = ref(null);
+    onMounted(() => {
+      const TE = new TEngine(threeTarget.value!);
+
+    })
+
+    return {
+      threeTarget
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="three-canvas" ref="threeTarget">
+
+  </div>
+</template>
+
+<style>
+.three-canvas {
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/assets/ts/TEngine.ts
@@ -0,0 +1,66 @@
+import { AmbientLight, AxesHelper, BoxBufferGeometry, GridHelper, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer,MOUSE } from "three";
+import Stats from 'three/examples/jsm/libs/stats.module'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+export class TEngine {
+
+  private dom: HTMLElement
+  private renderer: WebGLRenderer
+
+  private scene: Scene
+  private camera: PerspectiveCamera
+
+  constructor (dom: HTMLElement) {
+    this.dom = dom;
+    this.renderer = new WebGLRenderer();
+    this.scene = new Scene();
+    this.camera = new PerspectiveCamera(45, dom.offsetWidth / dom.offsetHeight, 1, 1000);
+
+    this.camera.position.set(20,20,20);
+    this.camera.lookAt(new Vector3(0,0,0));
+    this.camera.up = new Vector3(0,1,0);
+
+    this.renderer.setSize(dom.offsetWidth,dom.offsetHeight,true);
+
+    const box: Mesh = new Mesh(
+      new BoxBufferGeometry(10,10,10),
+      new MeshStandardMaterial({color: 'rgba(255,0,0)'})
+    )
+
+    const ambientLight: AmbientLight = new AmbientLight('rgba(255,255,255',1);
+
+    const axesHelper: AxesHelper = new AxesHelper(500);
+    const gridHelper: GridHelper = new GridHelper(500,20,'rgba(200,200,200)','rgba(100,100,100)');
+
+    this.scene.add(box);
+    this.scene.add(ambientLight);
+    this.scene.add(axesHelper);
+    this.scene.add(gridHelper);
+    // this.renderer.setClearColor('rgba(255,255,255)');
+    // this.renderer.clearColor();
+
+    // 初始性能监视器
+    const stats = Stats()
+    const statsDom = stats.domElement;
+    statsDom.style.position = 'fixed';
+    statsDom.style.position = '0';
+    statsDom.style.right = '5px';
+    statsDom.style.left = 'unSet';
+
+    // 初始arbitControls
+    const orbitControls: OrbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+    orbitControls.enableDamping = true;
+
+    const renderFunc = () => {
+      // box.position.x += 0.01;
+      box.rotateY(0.01);
+      orbitControls.update();
+      this.renderer.render(this.scene, this.camera);
+      stats.update();
+      requestAnimationFrame(renderFunc);
+    }
+    renderFunc();
+    dom.appendChild(this.renderer.domElement);
+    dom.appendChild(statsDom);
+
+  }
+}
